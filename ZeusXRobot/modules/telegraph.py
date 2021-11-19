b097837bbe2e9a326f5d61e5dc6e8d8e65c4efcf
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-11-19 20:37:50 +0530

    Update telegraph.py

--- ZeusXRobot/modules/telegraph.py
+++ ZeusXRobot/modules/telegraph.py
@@ -1,14 +1,14 @@
-from VegetaRobot.events import register
-from VegetaRobot import telethn as tbot
+from ZeusXRobot.events import register
+from ZeusXRobot import telethn as tbot
 TMP_DOWNLOAD_DIRECTORY = "./"
 from telethon import events
 import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-emilia = "EMILIA"
+zeus = "ZEUS"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=emilia)
+r = telegraph.create_account(short_name=zeus)
 auth_url = r["auth_url"]
 
 
