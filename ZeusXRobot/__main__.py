8a0d4b95443ad30a3cf4910666a2bb0cef04a3b6
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-20 00:34:28 +0530

    Update __main__.py

--- ZeusXRobot/__main__.py
+++ ZeusXRobot/__main__.py
@@ -230,6 +230,10 @@ def start(update: Update, context: CallbackContext):
                 IMPORTED["rules"].send_rules(update, args[0], from_pm=True)
 
         else:
+            update.effective_message.reply_sticker(
+                random.choice(STICKERS),
+                timeout=60,
+            )
             update.effective_message.reply_text(
                 PM_START_TEXT,
                 reply_markup=InlineKeyboardMarkup(buttons),
