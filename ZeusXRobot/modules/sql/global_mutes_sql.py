81e071d57a892cdf17495824660c51d343dd9109
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-19 20:40:32 +0530

    Update global_mutes_sql.py

--- ZeusXRobot/modules/sql/global_mutes_sql.py
+++ ZeusXRobot/modules/sql/global_mutes_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from VegetaRobot.modules.sql import BASE, SESSION
+from ZeusXRobot.modules.sql import BASE, SESSION
 
 
 class GloballyMutedUsers(BASE):
