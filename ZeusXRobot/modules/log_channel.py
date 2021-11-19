8c6b6264b7fd40557f0eb8dc850655f5636d4e0f
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-19 21:09:27 +0530

    Update log_channel.py

--- ZeusXRobot/modules/log_channel.py
+++ ZeusXRobot/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from VegetaRobot.modules.helper_funcs.misc import is_module_loaded
+from ZeusXRobot.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
