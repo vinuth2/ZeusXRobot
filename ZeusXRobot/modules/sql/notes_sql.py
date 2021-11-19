82bfdd129bf62917abf0ffe35a302229a33829e6
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-19 20:42:27 +0530

    Update notes_sql.py

--- ZeusXRobot/modules/sql/notes_sql.py
+++ ZeusXRobot/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from VegetaRobot.modules.helper_funcs.msg_types import Types
-from VegetaRobot.modules.sql import BASE, SESSION
+from ZeusXRobot.modules.helper_funcs.msg_types import Types
+from ZeusXRobot.modules.sql import BASE, SESSION
 from sqlalchemy import (Boolean, Column, Integer, String, UnicodeText, distinct,
                         func)
 
