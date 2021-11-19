e7a2236f031e2088b7c4ca216bbddb6016054bee
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-19 20:41:08 +0530

    Update locks_sql.py

--- ZeusXRobot/modules/sql/locks_sql.py
+++ ZeusXRobot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from VegetaRobot.modules.sql import SESSION, BASE
+from ZeusXRobot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
