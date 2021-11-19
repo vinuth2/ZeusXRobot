683ade81935d8f55af4d070dbd15650e3963260c
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-11-19 20:47:49 +0530

    Update purge.py

--- ZeusXRobot/modules/purge.py
+++ ZeusXRobot/modules/purge.py
@@ -1,8 +1,8 @@
 import time
 from telethon import events
 
-from VegetaRobot import telethn
-from VegetaRobot.modules.helper_funcs.telethn.chatstatus import (
+from ZeusXRobot import telethn
+from ZeusXRobot.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages, user_is_admin)
 
 
