cd7c794e5191b24f362f09a8d959f11238f9d277
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-19 20:43:03 +0530

    Update nsfw_sql.py

--- ZeusXRobot/modules/sql/nsfw_sql.py
+++ ZeusXRobot/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from VegetaRobot.modules.sql import BASE, SESSION
+from ZeusXRobot.modules.sql import BASE, SESSION
 #   |----------------------------------|
 #   |  Test Module by @EverythingSuckz |
 #   |        Kang with Credits         |
