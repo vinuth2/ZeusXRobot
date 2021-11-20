082989e97304b477bd9b7b628fbd7b2efd036173
Kishore <89995228+AASFCYBERKING@users.noreply.github.com>
2021-11-20 13:54:45 +0530

    Update __main__.py

--- ZeusXRobot/__main__.py
+++ ZeusXRobot/__main__.py
@@ -81,7 +81,7 @@ PM_START_TEXT = """
 ───『[GOD OF THUNDER](https://telegra.ph/file/f04c2af6c477eaa446c93.jpg)』───
 𝐇𝐞𝐲 𝐈'𝐦 [๖ۣۜZΞUS༻](https://en.m.wikipedia.org/wiki/Zeus)*,👋*
 𝐈 𝐀𝐦 𝐀 𝐆𝐫𝐞𝐞𝐤 𝐌𝐲𝐭𝐡𝐨𝐥𝐨𝐠𝐲 𝐆𝐨𝐝 
-𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩 𝐈 𝐖𝐢𝐥𝐥 𝐃𝐞𝐬𝐭𝐫𝐨𝐲 𝐄𝐯𝐢𝐥𝐬 𝐖𝐢𝐭𝐡 𝐌𝐲 𝐓𝐡𝐮𝐧𝐝𝐞𝐫 ⚡"""
+𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩 𝐈 𝐖𝐢𝐥𝐥 𝐃𝐞𝐬𝐭𝐫𝐨𝐲 𝐄𝐯𝐢𝐥𝐬 𝐖𝐢𝐭𝐡 𝐌𝐲 𝐓𝐡𝐮𝐧𝐝𝐞𝐫"""
 STICKERS = (
       "CAACAgUAAxkDAAPFYZf3AAHvMDnsv5fKW0FDznfWud9MAAK7BAAC0IDAVHm5KdsrXQNdIgQ", #1
       "CAACAgUAAxkDAAPeYZf5ijOHr2J-HyzS0P2W-RoCOW8AAsUEAALQgMBUHYrrhZZd-ogiBA", #2
