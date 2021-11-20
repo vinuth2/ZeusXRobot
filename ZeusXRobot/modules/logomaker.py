1de53e7edc0d6fdeddf3e2a6d8c8f4036926f0cd
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-20 06:09:29 +0530

    Update logomaker.py

--- ZeusXRobot/modules/logomaker.py
+++ ZeusXRobot/modules/logomaker.py
@@ -72,7 +72,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "black"
     shadowcolor = "blue"
-    fnt = glob.glob("./VegetaRobot/imagefiles/logofiles/*")
+    fnt = glob.glob("./ZeusXRobot/imagefiles/logofonts/*")
     randf = random.choice(fnt)
     font = ImageFont.truetype(randf, 140)
     w, h = draw.textsize(text, font=font)
