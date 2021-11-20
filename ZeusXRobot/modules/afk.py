28ca6d90e5944836477738346e3c02e7682ff927
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-20 18:56:41 +0530

    Update afk.py

--- ZeusXRobot/modules/afk.py
+++ ZeusXRobot/modules/afk.py
@@ -151,7 +151,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "✪AFK✪"
+__mod_name__ = "💼AFK"
 __command_list__ = ["afk"]
 __handlers__ = [(AFK_HANDLER, AFK_GROUP), (AFK_REGEX_HANDLER, AFK_GROUP),
                 (NO_AFK_HANDLER, AFK_GROUP),
