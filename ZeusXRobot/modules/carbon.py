78141a243afdf0d14d9f043664974070f1434f0b
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-23 22:25:03 +0530

    Create carbon.py

---
+++ ZeusXRobot/modules/carbon.py
@@ -0,0 +1,32 @@
+from pyrogram import filters
+
+from ZeusXRobot import pbot
+from ZeusXRobot.utils.errors import capture_err
+from ZeusXRobot.function.carbonfunc import make_carbon
+
+
+@p.on_message(filters.command("carbon"))
+@capture_err
+async def carbon_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "`Reply to a text message to make carbon.`"
+        )
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "`Reply to a text message to make carbon.`"
+        )
+    m = await message.reply_text("`Preparing Carbon`")
+    carbon = await make_carbon(message.reply_to_message.text)
+    await m.edit("`Uploading`")
+    await pbot.send_document(message.chat.id, carbon)
+    await m.delete()
+    carbon.close()
+
+__help__ = """
+  🔹 `/carbon` <text> [or reply]
+  
+Usage: Beautify your code using carbon.now.sh
+"""
+
+__mod_name__ = "CARBON🏷️"
