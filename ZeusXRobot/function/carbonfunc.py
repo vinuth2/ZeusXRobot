464af0d02730c56b7de5b6ae8f4174f15e63ce41
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-24 07:15:33 +0530

    Update carbonfunc.py

--- ZeusXRobot/function/carbonfunc.py
+++ ZeusXRobot/function/carbonfunc.py
@@ -19,5 +19,5 @@ async def make_carbon(code):
     url = "https://carbonara.vercel.app/api/cook"
     async with aiosession.post(url, json={"code": code}) as resp:
         image = BytesIO(await resp.read())
-    image.name = "mrjoker_carbon.png"
+    image.name = "Zeus_carbon.png"
     return image
