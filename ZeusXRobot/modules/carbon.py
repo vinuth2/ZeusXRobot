a50f6d44660a4b08896b318fe54be532beee70f4
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-24 22:04:20 +0530

    Update carbon.py

--- ZeusXRobot/modules/carbon.py
+++ ZeusXRobot/modules/carbon.py
@@ -18,9 +18,9 @@ async def carbon_func(_, message):
         return await message.reply_text(
             "`Reply to a text message to make carbon.`"
         )
-    m = await message.reply_text("`Preparing Carbon`")
+    m = await message.reply_text("`Makeing Carbon...`")
     carbon = await make_carbon(message.reply_to_message.text)
-    await m.edit("`Uploading`")
+    await m.edit("`Uploading...`")
     await pgram.send_document(message.chat.id, carbon)
     await m.delete()
     carbon.close()
