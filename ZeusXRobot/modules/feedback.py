06de4d368912b622be97dfc740d741541cd35469
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-26 22:14:29 +0530

    Update feedback.py

--- ZeusXRobot/modules/feedback.py
+++ ZeusXRobot/modules/feedback.py
@@ -16,10 +16,10 @@ def feedback(bot: Bot, update: Update):
   text = message.text[len('/feedback '):]
    
 
-  feed_text = f"Buddy's *New* feedback from [{name}](tg://user?id={userid})\n\nfeed: {text}"
+  feed_text = f"Zeus's *New* feedback from [{name}](tg://user?id={userid})\n\nfeed: {text}"
   
 
-  bot.send_message(-1001388164873, feed_text, parse_mode=ParseMode.MARKDOWN)
+  bot.send_message(-1001176690643, feed_text, parse_mode=ParseMode.MARKDOWN)
  
   text = html.escape(text)
   reply_text=f"Thankyou for giving us your feedback."
