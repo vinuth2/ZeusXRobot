de175adb012749236342f69d1c5fbdc5e84887df
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2021-12-29 19:25:36 +0530

    Update __main__.py

--- ZeusXRobot/__main__.py
+++ ZeusXRobot/__main__.py
@@ -335,7 +335,7 @@ def help_button(update, context):
         if mod_match:
             module = mod_match.group(1)
             text = (
-                "*Powered By @PegasusXteam*\nHere is the help for the *{}* module:\n".format(
+                "*Powered By @PegasusXteam*\n *Module Name:*`{}`\n".format(
                     HELPABLE[module].__mod_name__
                 )
                 + HELPABLE[module].__help__
