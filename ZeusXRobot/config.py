c3f298bbb23cb6c5f5871106fb03d325091e8fb7
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2021-12-29 15:34:48 +0530

    Update config.py

--- ZeusXRobot/config.py
+++ ZeusXRobot/config.py
@@ -20,7 +20,7 @@ class Config(object):
     TOKEN = "2116046406:AAFZ6LByMeijxz6nlcEtALmCw6SRBiiicVM"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1491497760  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "ctzfamily"
-    SUPPORT_CHAT = 'Zeussupportchat'  #Your own group for support, do not add the @
+    SUPPORT_CHAT = 'UnitedSupport'  #Your own group for support, do not add the @
     UPDATES_CHANNEL = 'ZeusUpdates' #Your own channel for Updates of bot, Do not add @
     JOIN_LOGGER = -1001574034400  #Prints any new group the bot is added to, prints just the name and ID.
     REM_BG_API_KEY = "http://removebg.com"
