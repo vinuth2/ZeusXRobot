6758d007aa91a48139c233eeab0555e5b41d1ac6
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2021-12-29 16:33:00 +0530

    Update chatbot.py

--- ZeusXRobot/modules/chatbot.py
+++ ZeusXRobot/modules/chatbot.py
@@ -7,9 +7,9 @@ from googletrans import Translator as google_translator
 from pyrogram import filters
 from aiohttp import ClientSession
 from ZeusXRobot import BOT_USERNAME as bu
-from ZeusXRobot import BOT_ID, pbot, arq
+from ZeusXRobot import BOT_ID, pgram as pbot, arq
 from ZeusXRobot.utils.chatbot import add_chat, get_session, remove_chat
-from ZeusXRobot.utils.pluginhelper import admins_only, edit_or_reply
+from ZeusXRobot.utils.wtf import admins_only, edit_or_reply
 
 url = "https://acobot-brainshop-ai-v1.p.rapidapi.com/get"
 
