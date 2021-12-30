daeb3514e63db714b138ee41fbb2b5af3bdcad58
Nandha ( <89440790+Ctzfamily@users.noreply.github.com>
2021-12-30 10:18:20 +0530

    Update config.py

--- ZeusXRobot/config.py
+++ ZeusXRobot/config.py
@@ -17,7 +17,7 @@ class Config(object):
 
     API_ID = 7126006  # integer value, dont use ""
     API_HASH = "f92b05be529835381859ead64a195fa2"
-    TOKEN = "2116046406:AAFZ6LByMeijxz6nlcEtALmCw6SRBiiicVM"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = "2116046406:AAHMDwkBKmiIdcBWIoizJ-urG9x_7hg1F0A"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1491497760  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "ctzfamily"
     SUPPORT_CHAT = 'UnitedSupport'  #Your own group for support, do not add the @
