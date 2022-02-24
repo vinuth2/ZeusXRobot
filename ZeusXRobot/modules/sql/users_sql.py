88545d3b50c87ad04d8ed06d2d49adcc30af06c6
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-24 06:32:21 +0530

    Update users_sql.py

--- ZeusXRobot/modules/sql/users_sql.py
+++ ZeusXRobot/modules/sql/users_sql.py
@@ -2,13 +2,13 @@ import threading
 
 from ZeusXRobot import dispatcher
 from ZeusXRobot.modules.sql import BASE, SESSION
-from sqlalchemy import (Column, ForeignKey, Integer, String, UnicodeText,
+from sqlalchemy import (Column, ForeignKey, BigInteger, String, UnicodeText,
                         UniqueConstraint, func)
 
 
 class Users(BASE):
     __tablename__ = "users"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     username = Column(UnicodeText)
 
     def __init__(self, user_id, username=None):
@@ -34,7 +34,7 @@ class Chats(BASE):
 
 class ChatMembers(BASE):
     __tablename__ = "chat_members"
-    priv_chat_id = Column(Integer, primary_key=True)
+    priv_chat_id = Column(BigInteger, primary_key=True)
     # NOTE: Use dual primary key instead of private primary key?
     chat = Column(
         String(14),
