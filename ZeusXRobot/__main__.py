f5e1955ee2e4fde98dc6a123a656e44eacc50d82
user name <98596498+vinuth2@users.noreply.github.com>
2022-03-20 00:53:17 +0530

    Update __main__.py

--- ZeusXRobot/__main__.py
+++ ZeusXRobot/__main__.py
@@ -117,7 +117,7 @@ Helpful commands:
 - /help: Sends this message; I'll tell you more about myself!
 - /donate: Gives you info on how to support me and my creator.
 If you want to report bugs or have any questions on how to use me then feel free to reach out: @ZeusSupportChat.
-All commands can be used with the following: / ! [.](https://telegra.ph/file/648aca75adfb9533a2161.jpg)
+All commands can be used with the following: / ! [.](https://telegra.ph/file/35b40ed60c1df7765dcdd.jpg)
 List of all the Modules:
 """.format(
     dispatcher.bot.first_name,
@@ -126,7 +126,7 @@ List of all the Modules:
 
 HELP_MSG = "Click the button below to get help manu in your pm."
 DONATE_STRING = """@ImVinuth"""
-HELP_IMG= "https://telegra.ph/file/648aca75adfb9533a2161.jpg"
+HELP_IMG= "https://telegra.ph/file/35b40ed60c1df7765dcdd.jpg"
 
 
 IMPORTED = {}
@@ -328,7 +328,7 @@ def help_button(update, context):
         if mod_match:
             module = mod_match.group(1)
             text = (
-                "*Powered By @PegasusXteam*\n *Module Name:*`{}`\n".format(
+                "*Powered by @The_Roboton*\n *Module Name:*`{}`\n".format(
                     HELPABLE[module].__mod_name__
                 )
                 + HELPABLE[module].__help__
