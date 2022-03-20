d963b01b127c1fa49e428f35861435ad1af34ef4
user name <98596498+vinuth2@users.noreply.github.com>
2022-03-20 09:08:36 +0530

    Update carbon.py

--- ZeusXRobot/modules/carbon.py
+++ ZeusXRobot/modules/carbon.py
@@ -14,7 +14,7 @@ async def carbon_func(_, message):
     m = await message.reply_text("**⬇Downloading...**")
     carbon = await make_carbon(message.reply_to_message.text)
     await m.edit("**⬆Uploading...**")
-    msg = "**Made by @ZeusXRobot**"
+    msg = "**Made by Marie**"
     await pbot.send_photo(message.chat.id, carbon,caption=msg)
     await m.delete()
     carbon.close()
