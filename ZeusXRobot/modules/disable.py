c1887e8d4c4a075f9b240c9805f41660964dd23e
user name <98596498+vinuth2@users.noreply.github.com>
2022-03-20 06:41:42 +0530

    Update disable.py

--- ZeusXRobot/modules/disable.py
+++ ZeusXRobot/modules/disable.py
@@ -344,7 +344,7 @@ if is_module_loaded(FILENAME):
     • `/listcmds`*:* list all possible toggleable commands
     """
 
-    __mod_name__ = "🔒Disable"
+    __mod_name__ = "🔒 Disabling"
 
 else:
     DisableAbleCommandHandler = CommandHandler
