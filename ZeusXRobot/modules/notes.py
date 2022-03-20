e61fe048567868cd04ac8905e1f6e4a85e828684
user name <98596498+vinuth2@users.noreply.github.com>
2022-03-20 06:46:51 +0530

    Update notes.py

--- ZeusXRobot/modules/notes.py
+++ ZeusXRobot/modules/notes.py
@@ -521,7 +521,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
 
 """
 
-__mod_name__ = "📄Notes"
+__mod_name__ = "📄 Notes"
 
 GET_HANDLER = CommandHandler("get", cmd_get)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get)
