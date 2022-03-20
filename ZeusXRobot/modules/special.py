8462d9df4b370e41d9d13556ccb7ec79bfbc7fcb
user name <98596498+vinuth2@users.noreply.github.com>
2022-03-20 07:29:08 +0530

    Update special.py

--- ZeusXRobot/modules/special.py
+++ ZeusXRobot/modules/special.py
@@ -75,7 +75,7 @@ Make me send a message to a specific chat.
   `/zombies clean`*:* remove a deleted acc in chat.
 """
 
-__mod_name__ = "✔Special"
+__mod_name__ = "😁 Special"
 
 SNIPE_HANDLER = CommandHandler(
     "snipe",
