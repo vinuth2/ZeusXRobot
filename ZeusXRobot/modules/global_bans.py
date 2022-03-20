a5afe87337224b0578555d82f0e8d3ee409f0f99
user name <98596498+vinuth2@users.noreply.github.com>
2022-03-20 07:35:44 +0530

    Update global_bans.py

--- ZeusXRobot/modules/global_bans.py
+++ ZeusXRobot/modules/global_bans.py
@@ -502,13 +502,12 @@ __help__ = f"""
 
 Anti-Spam, used by bot devs to ban spammers across all groups. This helps protect \
 you and your groups by removing spam flooders as quickly as possible.
-*Note:* Users can appeal gbans or report spammers at @{SUPPORT_CHAT}
+*Note:* Users can appeal gbans or report spammers at @MusicUpdates_chat
 
-This also integrates @Spamwatch API to remove Spammers as much as possible from your chatroom!
+This also integrates Spam watch API to remove Spammers as much as possible from your chatroom!
 *What is SpamWatch?*
 SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/f584b643c6f4be0b1de53.jpg)
 Constantly help banning spammers off from your group automatically So, you wont have to worry about spammers storming your group.
-*Note:* Users can appeal spamwatch bans at @SpamwatchSupport
 """
 
 GBAN_HANDLER = CommandHandler("gban", gban)
