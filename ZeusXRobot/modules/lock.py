6bfba62a18f983db7f4430b9fbbddfb51d417ee8
user name <98596498+vinuth2@users.noreply.github.com>
2022-04-08 17:44:35 +0530

    Update lock.py

--- ZeusXRobot/modules/lock.py
+++ ZeusXRobot/modules/lock.py
@@ -646,7 +646,7 @@ Locking bots will stop non-admins from adding bots to the chat.
  • Unlocking permission *pin* will allow members (non-admins) to pinned a message in a group
 """
 
-__mod_name__ = "🔐 Locks"
+__mod_name__ = "Locks"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler(
