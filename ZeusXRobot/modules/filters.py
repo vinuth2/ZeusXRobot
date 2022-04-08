cebd7af855d79a04024bd1a83972047a89920ce3
user name <98596498+vinuth2@users.noreply.github.com>
2022-04-08 17:41:23 +0530

    Update filters.py

--- ZeusXRobot/modules/filters.py
+++ ZeusXRobot/modules/filters.py
@@ -621,7 +621,7 @@ Check `/markdownhelp` to know more!
 
 """
 
-__mod_name__ = "💫 Filters"
+__mod_name__ = "Filters"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
