59cf99495bd076d7228f854620a0c4ea780fa6cb
user name <98596498+vinuth2@users.noreply.github.com>
2022-04-08 17:36:00 +0530

    Update antiflood.py

--- ZeusXRobot/modules/antiflood.py
+++ ZeusXRobot/modules/antiflood.py
@@ -354,7 +354,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "📛 Anti-Flood"
+__mod_name__ = "Antiflood"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood)
